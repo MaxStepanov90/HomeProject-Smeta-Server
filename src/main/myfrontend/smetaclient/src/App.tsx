bcb837f0915aabcd7734e669d211931db1361b74
Max Stepanov <mu5cool@yandex.ru>
2020-07-06 02:28:54 +0300

    Ref

--- src/main/myfrontend/smetaclient/src/App.tsx
+++ src/main/myfrontend/smetaclient/src/App.tsx
@@ -6,7 +6,6 @@ import Col from "react-bootstrap/Col";
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom'
 import ProjectList from "./components/Project/projectList/ProjectList";
 import ProjectForm from "./components/Project/projectForm/ProjectForm";
-import {TabContainer} from "react-bootstrap";
 import ProjectInfo from "./components/Project/projectInfo/ProjectInfo";
 import {Welcome} from "./components/Welcome/Welcome";
 import EstimateDetailsTabs from "./components/EstimateDetail/Tabs/EstimateDetailsTabs";
@@ -17,6 +16,7 @@ import PaymentForm from "./components/Payment/paymentForm/PaymentForm";
 import MarkUpList from "./components/MarkUp/markUpList/MarkUpList";
 import MarkUpForm from "./components/MarkUp/markUpForm/MarkUpForm";
 import React from "react";
+import TabContainer from "react-bootstrap/TabContainer";
 
 function App() {
     return (
