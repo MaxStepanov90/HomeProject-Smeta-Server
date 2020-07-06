31d264da1bea65a5de071e32fc394bda360033bf
Max Stepanov <mu5cool@yandex.ru>
2020-07-06 10:49:47 +0300

    Ref4

--- src/main/myfrontend/smetaclient/src/service/store/store.js
+++ src/main/myfrontend/smetaclient/src/service/store/store.js
@@ -3,6 +3,6 @@ import thunk from "redux-thunk";
 import {rootReducer} from "../reducers/rootReducer";
 
 const store = createStore(rootReducer,
-    compose(applyMiddleware(thunk), window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__())
+    compose(applyMiddleware(thunk))
 )
 export default store;
