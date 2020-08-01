b84ad791c120be0d699035ae9d1301508c98daa7
Max Stepanov <mu5cool@yandex.ru>
2020-08-01 17:37:16 +0300

    Добавил перевод Google translate

--- src/main/myfrontend/smetaclient/src/components/Generic/NavigationBar.tsx
+++ src/main/myfrontend/smetaclient/src/components/Generic/NavigationBar.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import {Nav, Navbar} from "react-bootstrap";
 import {Link} from "react-router-dom";
+import NavDropdown from "react-bootstrap/NavDropdown";
 
 
 export const NavigationBar: React.FC = () => {
@@ -16,6 +17,16 @@ export const NavigationBar: React.FC = () => {
                     <Link to={"/markUps"} className="nav-link">Наценки</Link>
                     <Link to={"/info"} className="nav-link">Контакты</Link>
                 </Nav>
+                <NavDropdown className="nav-link" title="Выберите язык" id="basic-nav-dropdown">
+                    <NavDropdown.Item data-google-lang="ru" className="language__img">русский</NavDropdown.Item>
+                    <NavDropdown.Item data-google-lang="en" className="language__img">английский</NavDropdown.Item>
+                    <NavDropdown.Item data-google-lang="de" className="language__img">немецкий</NavDropdown.Item>
+                    <NavDropdown.Item data-google-lang="fr" className="language__img">французский</NavDropdown.Item>
+                    <NavDropdown.Item data-google-lang="pt" className="language__img">португальский</NavDropdown.Item>
+                    <NavDropdown.Item data-google-lang="es" className="language__img">испанский</NavDropdown.Item>
+                    <NavDropdown.Item data-google-lang="it" className="language__img">итальянский</NavDropdown.Item>
+                    <NavDropdown.Item data-google-lang="ar" className="language__img">арабский</NavDropdown.Item>
+                </NavDropdown>
             </Navbar.Collapse>
         </Navbar>
     )
